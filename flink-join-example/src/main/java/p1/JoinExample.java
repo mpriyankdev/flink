6f81e72a56948da2cf404f87732fcaaa48b784b7
mpriyankdev <mpriyank.dev@gmail.com>
2020-08-24 23:19:28 +0530

    join example : inner , left , right and outer

--- flink-join-example/src/main/java/p1/JoinExample.java
+++ flink-join-example/src/main/java/p1/JoinExample.java
@@ -7,6 +7,7 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.core.fs.FileSystem;
 
 public class JoinExample {
     public static void main(String[] args) throws Exception {
@@ -38,6 +39,7 @@ public class JoinExample {
 
         // join datasets on person_id
         // joined format will be <id, person_name, state>
+        //similarly leftOuterJoin and rightOuterJoin can be implemented
         DataSet<Tuple3<Integer, String, String>> joined = personSet.join(locationSet).where(0).equalTo(0)
                 .with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>>() {
 
@@ -46,7 +48,59 @@ public class JoinExample {
                     }
                 });
 
-        joined.writeAsCsv(params.get("output"), "\n", " ");
+        // left outer join datasets on person_id
+        // joined format will be <id, person_name, state>
+
+        DataSet<Tuple3<Integer, String, String>> leftOuterJoin = personSet.leftOuterJoin(locationSet).where(0).equalTo(0)
+                .with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>>() {
+                    public Tuple3<Integer, String, String> join(Tuple2<Integer, String> person, Tuple2<Integer, String> location) {
+                        // check for nulls
+                        if (location == null) {
+                            return new Tuple3<Integer, String, String>(person.f0, person.f1, "NULL");
+                        }
+
+                        return new Tuple3<Integer, String, String>(person.f0, person.f1, location.f1);
+                    }
+                });
+
+        DataSet<Tuple3<Integer, String, String>> rightOuterjoin = personSet.rightOuterJoin(locationSet).where(0).equalTo(0)
+                .with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>>() {
+
+                    public Tuple3<Integer, String, String> join(Tuple2<Integer, String> person, Tuple2<Integer, String> location) {
+                        // check for nulls
+                        if (person == null) {
+                            return new Tuple3<Integer, String, String>(location.f0, "NULL", location.f1);
+                        }
+
+                        return new Tuple3<Integer, String, String>(person.f0, person.f1, location.f1);
+                    }
+                });
+
+        // joined format will be <id, person_name, state>
+
+        DataSet<Tuple3<Integer, String, String>> fullOuterJoin = personSet.fullOuterJoin(locationSet).where(0).equalTo(0)
+                .with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>>() {
+
+                    public Tuple3<Integer, String, String> join(Tuple2<Integer, String> person, Tuple2<Integer, String> location) {
+                        // check for nulls
+                        if (location == null) {
+                            return new Tuple3<Integer, String, String>(person.f0, person.f1, "NULL");
+                        }
+                        // for rightOuterJoin
+                        else if (person == null)
+                            return new Tuple3<Integer, String, String>(location.f0, "NULL", location.f1);
+
+                        return new Tuple3<Integer, String, String>(person.f0, person.f1, location.f1);
+                    }
+                });
+
+        //currently writing only inner-join example
+        //enabling the feature of overwrite , can be removed by using some other overloaded method
+        joined.writeAsCsv(params.get("output"), "\n", " ",FileSystem.WriteMode.OVERWRITE);
+        leftOuterJoin.writeAsCsv(params.get("output"), "\n", " " , FileSystem.WriteMode.OVERWRITE);
+        rightOuterjoin.writeAsCsv(params.get("output"), "\n", " ",FileSystem.WriteMode.OVERWRITE);
+        fullOuterJoin.writeAsCsv(params.get("output"), "\n", " ",FileSystem.WriteMode.OVERWRITE);
+
 
         env.execute("Join example");
     }
